7ebb10161168e58deb1940bfe446396e722cfc8e
Darma Yasa <darmayasa@192.168.204.100>
2022-07-10 13:31:43 +0800

    implement RegisterUser

--- src/Applications/use_cases/RegisterUserUseCase.js
+++ src/Applications/use_cases/RegisterUserUseCase.js
@@ -1,5 +1,17 @@
-class RegisterUserUseCase {
+const UserRegister = require('../../Domains/users/entities/UserRegister');
 
+class RegisterUserUseCase {
+  constructor({ userRepository, passwordHash }) {
+    this._userRepository = userRepository;
+    this._passwordHash = passwordHash;
+  }
+
+  async execute(payload) {
+    const user = new UserRegister(payload);
+    await this._userRepository.verifyAvailableKtp(user.ktp);
+    user.password = this._passwordHash.hash(user.ktp);
+    await this._userRepository.registerUser(user);
+  }
 }
 
 module.exports = RegisterUserUseCase;
--- src/Infrastructures/repository/UserRepositoryPostgres.js
+++ src/Infrastructures/repository/UserRepositoryPostgres.js
@@ -1,5 +1,41 @@
-class UserRepositoryPostgres {
+/* eslint-disable camelcase */
+const UserRepository = require('../../Domains/users/UserRepository');
 
+class UserRepositoryPostgres extends UserRepository {
+  constructor(pool, idGenerator) {
+    super();
+    this._pool = pool;
+    this._idGenerator = idGenerator;
+  }
+
+  async registerUser(payload) {
+    const {
+      name,
+      ktp,
+      current_address,
+      old_address,
+      created_at,
+      updated_at,
+    } = payload;
+    const id = `user-${this._idGenerator()}`;
+    const query = {
+      text: `INSERT INTO users
+      VALUES($1,$2,$3,$4,$5,$6,$7)`,
+      vlaues: [id, name, ktp, current_address, old_address, created_at, updated_at],
+    };
+
+    await this._pool.query(query);
+  }
+
+  async verifyAvailableKtp(ktp) {
+    const query = {
+      text: `SELECT id
+      FROM users
+      WHERE ktp = $1`,
+      values: [ktp],
+    };
+    await this.pool.query(query);
+  }
 }
 
 module.exports = UserRepositoryPostgres;
