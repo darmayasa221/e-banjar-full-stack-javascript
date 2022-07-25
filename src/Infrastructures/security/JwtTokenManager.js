44a162a7e0f2d5b6f8e5a959164d10f61e6fa3a0
Darma Yasa <darmayasa@192.168.204.104>
2022-07-25 10:13:35 +0800

    authentications

---
+++ src/Infrastructures/security/JwtTokenManager.js
@@ -0,0 +1,33 @@
+const AuthenticationTokenManager = require('../../Applications/security/AuthenticationTokenManager');
+const InvariantError = require('../../Commons/exceptions/InvariantError');
+
+class JwtTokenManager extends AuthenticationTokenManager {
+  constructor(jwt) {
+    super();
+    this._jwt = jwt;
+  }
+
+  async createAccessToken(payload) {
+    return this._jwt.generate(payload, process.env.ACCESS_TOKEN_KEY);
+  }
+
+  async createRefreshToken(payload) {
+    return this._jwt.generate(payload, process.env.REFRESH_TOKEN_KEY);
+  }
+
+  async verifyRefreshToken(token) {
+    try {
+      const artifacts = this._jwt.decode(token);
+      this._jwt.verify(artifacts, process.env.REFRESH_TOKEN_KEY);
+    } catch (error) {
+      throw new InvariantError('Refresh Token not valid');
+    }
+  }
+
+  async decodePayload(payload) {
+    const artifacts = this._jwt.decode(payload);
+    return artifacts.decoded.payload;
+  }
+}
+
+module.exports = JwtTokenManager;
---
+++ src/Infrastructures/security/_test/JwtTokenManager.test.js
@@ -0,0 +1,86 @@
+const Jwt = require('@hapi/jwt');
+const InvariantError = require('../../../Commons/exceptions/InvariantError');
+const JwtTokenManager = require('../JwtTokenManager');
+
+describe('JwtTokenManager', () => {
+  describe('createAccessToken Function', () => {
+    it('should create accessToken correctly', async () => {
+      // Arrange
+      const payload = {
+        ktp: 1234567890123456,
+        name: 'jhon',
+      };
+      const mockJwtToken = {
+        generate: jest.fn().mockImplementation(() => 'mock_token'),
+      };
+      const jwtTokenManager = new JwtTokenManager(mockJwtToken);
+      // action
+      const accessToken = await jwtTokenManager.createAcceessToken(payload);
+      // Assert
+      expect(mockJwtToken.generate).toBeCalledWith(payload, process.env.ACCESS_TOKEN_KEY);
+      expect(accessToken).toEqual('mock_token');
+    });
+  });
+  describe('refreshToken Function', () => {
+    it('should create refreshToken correctly', async () => {
+      // Arange
+      const payload = {
+        ktp: 1234567890123456,
+        name: 'jhon',
+      };
+      const mockJwtToken = {
+        generate: jest.fn().mockImplementation(() => 'mock_token'),
+      };
+      const jwtTokenManager = new JwtTokenManager(mockJwtToken);
+      // Action
+      const refreshToken = await jwtTokenManager.createRefreshToken(payload);
+      // Action
+      expect(mockJwtToken.generate).toBeCalledWith(payload, process.env.REFRESH_TOKEN_KEY);
+      expect(refreshToken).toEqual('mock_token');
+    });
+  });
+  describe('verifyRefreshToken Function', () => {
+    it('should throw InvariantError when verification failed', async () => {
+      // Arrange
+      const jwtTokenManager = new JwtTokenManager(Jwt.token);
+      const accessToken = await jwtTokenManager.createAcceessToken({
+        ktp: 1234567890123456,
+        name: 'jhon',
+      });
+      // Action and Assert
+      await expect(jwtTokenManager.verifyRefreshToken(accessToken))
+        .rejects
+        .toThrow(InvariantError);
+    });
+    it('should not throw InvariantError when refresh token token verified', async () => {
+      // Arrange
+      const jwtTokenManager = new JwtTokenManager(Jwt.token);
+      const refreshToken = await jwtTokenManager.createRefreshToken({
+        ktp: 1234567890123456,
+        name: 'jhon',
+      });
+      // Action and Assert
+      await expect(jwtTokenManager.verifyRefreshToken(refreshToken))
+        .resolves
+        .not.toThrow(InvariantError);
+    });
+  });
+  describe('decodePayload Function', () => {
+    it('should decode payload correctly', async () => {
+      // Arrange
+      const jwtTokenManager = new JwtTokenManager(Jwt.token);
+      const accessToken = await jwtTokenManager.createAcceessToken({
+        ktp: 1234567890123456,
+        name: 'jhon',
+      });
+      // Action
+      const {
+        ktp: expectedKtp,
+        name: expectedName,
+      } = await jwtTokenManager.decodePayload(accessToken);
+      // Assert
+      expect(expectedKtp).toEqual(1234567890123456);
+      expect(expectedName).toEqual('jhon');
+    });
+  });
+});
