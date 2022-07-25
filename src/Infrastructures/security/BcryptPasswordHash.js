91413cbc72d2b6adc76a82e07e32af5090c6f468
Darma Yasa <darmayasa@192.168.204.104>
2022-07-25 16:05:17 +0800

    password hash implement and exception Authentication error

--- src/Infrastructures/security/BcryptPasswordHash.js
+++ src/Infrastructures/security/BcryptPasswordHash.js
@@ -1,7 +1,7 @@
 const PasswordHash = require('../../Applications/security/PasswordHash');
+const AuthenticationError = require('../../Commons/exceptions/AuthenticationError');
 
 class BcryptPasswordHash extends PasswordHash {
-
   constructor(bcyrpt, saltRound = 10) {
     super();
     this._bcyrpt = bcyrpt;
@@ -10,6 +10,13 @@ class BcryptPasswordHash extends PasswordHash {
 
   async hash(password) {
     return this._bcyrpt.hash(password.toString(), this._saltRound);
+  }
+
+  async comparePassword(password, hashedPassword) {
+    const match = await this._bcyrpt.compare(password.toString(), hashedPassword);
+    if (!match) {
+      throw new AuthenticationError('password yang dimasukkan salah');
+    }
   }
 }
 
