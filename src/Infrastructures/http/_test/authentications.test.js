fb82a80e66cb30e031dcdd900dc374b9684720fe
Darma Yasa <darmayasa@192.168.204.104>
2022-07-26 00:27:24 +0800

    authentication E2E testimg

---
+++ src/Infrastructures/http/_test/authentications.test.js
@@ -0,0 +1,136 @@
+const AuthenticationsTableTestHelper = require('../../../../tests/AuthenticationsTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const container = require('../../container');
+const pool = require('../../database/postgres/pool');
+const createServer = require('../createServer');
+
+describe('/authentications endpoint', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+  afterEach(async () => {
+    Promise.all([
+      await UsersTableTestHelper.cleanTable(),
+      await AuthenticationsTableTestHelper.cleanTable(),
+    ]);
+  });
+  describe('when POST /authentications', () => {
+    it('should response 201 and new authentication', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 1234567890123456,
+        password: 1234567890123456,
+      };
+      const server = await createServer(container);
+      // add user
+      await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: {
+          name: 'darma',
+          ktp: 1234567890123456,
+          current_address: 'alamat sekarang',
+          old_address: 'alamat sebelumnya',
+        },
+      });
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: requestPayload,
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.accessToken).toBeDefined();
+      expect(responseJson.data.refreshToken).toBeDefined();
+    });
+    it('should response 400 if username not found', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 1234567890123456,
+        password: 1234567890123456,
+      };
+      const server = await createServer(container);
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: requestPayload,
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('username tidak ditemukan');
+    });
+    it('should response 401 if password wrong', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 1234567890123456,
+        password: 1234567890123455,
+      };
+      const server = await createServer(container);
+      // add user
+      await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: {
+          name: 'darma',
+          ktp: 1234567890123456,
+          current_address: 'alamat sekarang',
+          old_address: 'alamat sebelumnya',
+        },
+      });
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: requestPayload,
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(401);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('password salah');
+    });
+    it('should response 400 if login payload did not contain needed property', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 1234567890123456,
+      };
+      const server = await createServer(container);
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: requestPayload,
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('username dan password tidak boleh kosong');
+    });
+    it('should response 400 if login payload did not meet data type specification', async () => {
+      // Arrange
+      const requestPayload = {
+        username: '1234567890123456',
+        password: 1234567890123455,
+      };
+      const server = await createServer(container);
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/authentications',
+        payload: requestPayload,
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('masukkan username dan password dengan data yang benar');
+    });
+  });
+});
---
+++ src/Infrastructures/http/_test/users.test.js
@@ -0,0 +1,121 @@
+/* eslint-disable no-loss-of-precision */
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const container = require('../../container');
+const pool = require('../../database/postgres/pool');
+const createServer = require('../createServer');
+
+describe('/users endpoint', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+  afterEach(async () => {
+    await UsersTableTestHelper.cleanTable();
+  });
+  describe('when POST /users', () => {
+    it('should response 201 and persisted user', async () => {
+      // Arrange
+      const requestPayload = {
+        name: 'darma',
+        ktp: 1234567890123456,
+        current_address: 'alamat sekarang',
+        old_address: 'alamat sebelumnya',
+      };
+      const server = await createServer(container);
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.message).toEqual('Terimakasih sudah melakukan pendaftaran di E-banjar');
+    });
+    it('should response 400 when request payload did not contain needed property', async () => {
+      // Arrange
+      const requestPayload = {
+        name: 'darma',
+        current_address: 'alamat sekarang',
+        old_address: 'alamat sebelumnya',
+      };
+      const server = await createServer(container);
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('registrasi gagal. mohon untuk melengkapi semua form');
+    });
+    it('sshould response 400 when request payload did not meet data type specification', async () => {
+      // Arrange
+      const requestPayload = {
+        name: 'darma',
+        ktp: '1234567890123456',
+        current_address: 'alamat sekarang',
+        old_address: 'alamat sebelumnya',
+      };
+      const server = await createServer(container);
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('registrasi gagal. tipe data yang dimasukan tidak sesuai');
+    });
+    it('should respone 400 when ktp length is > 16', async () => {
+      // Arrange
+      const requestPayload = {
+        name: 'darma',
+        ktp: 12345678901234567,
+        current_address: 'alamat sekarang',
+        old_address: 'alamat sebelumnya',
+      };
+      const server = await createServer(container);
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('registrasi gagal. jumlah ktp melebihi 16 digit');
+    });
+    it('should response 400 when ktp available', async () => {
+      // Arrange
+      await UsersTableTestHelper.registerUser({ ktp: 1234567890123456 });
+      const requestPayload = {
+        name: 'darma',
+        ktp: 1234567890123456,
+        current_address: 'alamat sekarang',
+        old_address: 'alamat sebelumnya',
+      };
+      const server = await createServer(container);
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual('registrasi gagal. ktp yang dimasukkan salah');
+    });
+  });
+});
