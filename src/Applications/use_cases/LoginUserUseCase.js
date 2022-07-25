c7e93afc0106ae5fb1b4d34ed5b96b3233d90b44
Darma Yasa <darmayasa@Darmas-MacBook-Pro.local>
2022-07-25 14:00:07 +0800

    loginUserUseCase and userRepository testing

---
+++ src/Applications/use_cases/LoginUserUseCase.js
@@ -0,0 +1,5 @@
+class LoginUserUseCase {
+
+}
+
+module.exports = LoginUserUseCase;
--- src/Domains/users/_test/UserRepository.test.js
+++ src/Domains/users/_test/UserRepository.test.js
@@ -6,6 +6,8 @@ describe('UserRepository Abstract', () => {
     const userRepository = new UserRepository();
     // Action and Assert
     await expect(userRepository.registerUser({})).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    await expect(userRepository.verifyAvailableKtp()).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(userRepository.verifyAvailableKtp('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(userRepository.getNameByKtp('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(userRepository.getPasswordByKtp('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
 });
