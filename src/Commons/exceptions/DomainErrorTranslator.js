e89d4b0a59e0cf5671070a69d022f143c1d8bf50
Darma Yasa <darmayasa@192.168.204.104>
2022-07-26 11:47:30 +0800

    domain error implement

--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -10,7 +10,7 @@ DomainErrorTranslator._directories = {
   'USER_REGISTER.NOT_CONTAIN_NEEDED_PROPERT': new InvariantError('registrasi gagal. mohon untuk melengkapi semua form'),
   'USER_REGISTER.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('registrasi gagal. tipe data yang dimasukan tidak sesuai'),
   'USER_REGISTER.KTP_OF_LENGTH_GREETER_THEN_16': new InvariantError('registrasi gagal. jumlah ktp melebihi 16 digit'),
-  'NEW_AUTH.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('username dan password tidak boleh kosong'),
-  'NEW_AUTH.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('masukkan username dan password dengan data yang benar'),
+  'USER_LOGIN.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('username dan password tidak boleh kosong'),
+  'USER_LOGIN.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('masukkan username dan password dengan data yang benar'),
 };
 module.exports = DomainErrorTranslator;
