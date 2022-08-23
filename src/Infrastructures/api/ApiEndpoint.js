036a2793229406e930928de232209873ba1d142f
Darma Yasa <darmayasa@192.168.204.104>
2022-08-23 12:54:55 +0800

    push

--- src/Infrastructures/api/ApiEndpoint.js
+++ src/Infrastructures/api/ApiEndpoint.js
@@ -1,5 +1,5 @@
 const API_ENDPOINT = {
-  API_EBANJAR: 'http://13.212.144.88:5000',
+  API_EBANJAR: 'http://ec2-13-212-144-88.ap-southeast-1.compute.amazonaws.com:5000',
 };
 
 module.exports = API_ENDPOINT;
